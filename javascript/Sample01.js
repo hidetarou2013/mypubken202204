46141d7903a150785ca9dd9121a174bc6918ba19
Hidetoshi Maekawa <hide1227@gmail.com>
2022-04-08 13:09:15 +0900

    Sample01:7:文字入力があった場合にリストに追加する

--- javascript/Sample01.js
+++ javascript/Sample01.js
@@ -1,2 +1,29 @@
 // コメント
-console.log('Hello World');
+console.log('This is Sample01 Javascript');
+
+// ユーザがテキストフィールド内で何か入力し、ENTERしたタイミングで値を取り出す
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+// form内でENTER押されたら
+form.addEventListener("submit",function (event){
+    console.log(input.value);
+    // defaultのイベントを無効にする。⇒reloadを停止
+    event.preventDefault();
+    
+    if(input.value.length > 0){
+        add();
+    }
+
+
+});
+
+// Listに追加
+function add(){
+    const li = document.createElement("li")
+    li.innerText = input.value;
+    li.classList.add("list-group-item");
+    ul.appendChild(li);
+    input.value = "";
+}
