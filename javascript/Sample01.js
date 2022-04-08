0df3eb3957f5c936767fdc4c9611ea791e34382b
Hidetoshi Maekawa <hide1227@gmail.com>
2022-04-08 13:47:29 +0900

    Sample01:9:localStorage保存と取得

--- javascript/Sample01.js
+++ javascript/Sample01.js
@@ -6,6 +6,14 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+// localStorageから取得
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+    todos.forEach(todo => {
+        add(todo);
+    });
+}
+
 // form内でENTER押されたら
 form.addEventListener("submit",function (event){
     console.log(input.value);
@@ -20,11 +28,34 @@ form.addEventListener("submit",function (event){
 });
 
 // Listに追加
-function add(){
+function add(todo){
     let todoText = input.value;
+    if(todo){
+        todoText = todo;
+    }
     const li = document.createElement("li")
     li.innerText = todoText;
     li.classList.add("list-group-item");
     ul.appendChild(li);
     input.value = "";
+    saveData();
+}
+
+// localStorageに保存
+function saveData(){
+    const lists = document.querySelectorAll("li.list-group-item");
+    let todos = [];
+    console.log(lists);
+    //localStorage.add();
+    lists.forEach(value => {
+        todos.push(value.innerText)
+    });
+    console.log(todos);
+    console.log(JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// localStorageから取り出す
+function aaa(){
+
 }
